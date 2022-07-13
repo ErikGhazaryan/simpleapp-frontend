0e0ee29e33c253f1f306e7811449df6467fc448a
Erik Ghazaryan <erikghazarian@gmail.com>
2022-07-13 17:28:34 +0200

    added amazon link

--- src/app/common/global-variables.ts
+++ src/app/common/global-variables.ts
@@ -1,6 +1,6 @@
 export class GlobalVariables {
 
-    //    static baseUrlConst: string = 'http://localhost:5000/api';
-        static baseUrlConst: string = 'http://localhost:8080/api';
-   // static baseUrlConst: string = 'http://cardockerdomain.us-east-1.elasticbeanstalk.com/api';
+   //     static baseUrlConst: string = 'http://localhost:5000/api';
+   //     static baseUrlConst: string = 'http://localhost:8080/api';
+    static baseUrlConst: string = 'http://Simpleapplication-env.eba-utrmecry.us-east-1.elasticbeanstalk.com/api';
       }
